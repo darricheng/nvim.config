c5683c54b2737e7e931031a95ebb1f8e8b6da183
Darric Heng <darricheng@gmail.com>
2023-04-03 16:23:12 +0800

    Debug and cleanup

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,8 +11,33 @@ lsp.ensure_installed({
     'svelte',
 })
 
+lsp_config.lua_ls.setup({
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' }
+            },
+            workspace = {
+                checkThirdParty = false,
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = { enable = false },
+        },
+    }
+})
+
 lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
+
+    -- Format on save
+    lsp.buffer_autoformat()
 
     local map = function(mode, keys, func, desc)
         if desc then
@@ -49,25 +74,6 @@ lsp.on_attach(function(_, bufnr)
     end, { desc = 'Format current buffer with LSP' })
 end)
 
-lsp_config.lua_ls.setup({
-    Lua = {
-        runtime = {
-            -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-            version = 'LuaJIT',
-        },
-        diagnostics = {
-            -- Get the language server to recognize the `vim` global
-            globals = { 'vim' }
-        },
-        workspace = {
-            checkThirdParty = false,
-            -- Make the server aware of Neovim runtime files
-            library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = { enable = false },
-    },
-})
 
 
 lsp.setup()
