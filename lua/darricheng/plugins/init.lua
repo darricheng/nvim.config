4114054faf48dfcf37b69aa5a99f6505ddedb5bc
Darric Heng <darricheng@gmail.com>
2023-04-10 20:11:38 +0800

    Split colorschemes into separate files

--- lua/darricheng/plugins/init.lua
+++ lua/darricheng/plugins/init.lua
@@ -24,16 +24,6 @@ return {
       },
     },
   },
-
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
