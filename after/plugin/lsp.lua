e704a303fff104da13876f253b996d29b6f7aafb
Darric Heng <darricheng@gmail.com>
2023-04-16 22:31:46 +0800

    Remove auto select of cmp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -96,7 +96,7 @@ cmp.setup({
         ['<C-d>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
-        ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = true }),
+        ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
         ['<Tab>'] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
