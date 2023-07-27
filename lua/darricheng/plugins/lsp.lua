28a1e37a6981e200dcbfa8c0bab47f4587188320
Darric Heng <darric-sg@zto.com>
2023-07-27 11:03:34 +0800

    fidget will soon be rewritten, update tag to not break

--- lua/darricheng/plugins/lsp.lua
+++ lua/darricheng/plugins/lsp.lua
@@ -29,7 +29,11 @@ return {
   { 'simrat39/rust-tools.nvim' },
   -- Useful status updates for LSP
   -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-  { 'j-hui/fidget.nvim',       opts = {} },
+  {
+    'j-hui/fidget.nvim',
+    tag = "legacy",
+    opts = {}
+  },
   -- Additional lua configuration, makes nvim stuff amazing!
   { 'folke/neodev.nvim' },
 }
