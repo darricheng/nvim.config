34d5f3b65e56a9bce5f70310d25ad3a75708aca7
Darric Heng <darricheng@gmail.com>
2023-09-20 21:17:11 +0800

    split out dap config following kickstart

---
+++ lua/darricheng/plugins/dap.lua
@@ -0,0 +1,18 @@
+return {
+  -- debug.lua
+  --
+  -- Shows how to use the DAP plugin to debug your code.
+  'mfussenegger/nvim-dap',
+  -- NOTE: And you can specify dependencies as well
+  dependencies = {
+    -- Creates a beautiful debugger UI
+    'rcarriga/nvim-dap-ui',
+
+    -- Installs the debug adapters for you
+    'williamboman/mason.nvim',
+    'jay-babu/mason-nvim-dap.nvim',
+
+    -- Add your own debuggers here
+    'leoluz/nvim-dap-go',
+  },
+}
--- lua/darricheng/plugins/init.lua
+++ lua/darricheng/plugins/init.lua
@@ -94,24 +94,6 @@ return {
     end,
   },
   {
-    -- debug.lua
-    --
-    -- Shows how to use the DAP plugin to debug your code.
-    'mfussenegger/nvim-dap',
-    -- NOTE: And you can specify dependencies as well
-    dependencies = {
-      -- Creates a beautiful debugger UI
-      'rcarriga/nvim-dap-ui',
-
-      -- Installs the debug adapters for you
-      'williamboman/mason.nvim',
-      'jay-babu/mason-nvim-dap.nvim',
-
-      -- Add your own debuggers here
-      'leoluz/nvim-dap-go',
-    },
-  },
-  {
     'folke/trouble.nvim',
     dependencies = 'nvim-tree/nvim-web-devicons',
   },
