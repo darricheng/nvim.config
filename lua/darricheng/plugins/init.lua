e7a71e539b44be270f2277fbc6f12c61d7fbbeb5
Darric Heng <darricheng@gmail.com>
2023-10-02 21:13:39 +0800

    remove unnecessary config for indent_blankline

--- lua/darricheng/plugins/init.lua
+++ lua/darricheng/plugins/init.lua
@@ -46,12 +46,6 @@ return {
     -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     main = 'ibl',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
   },
 
   -- "gc" to comment visual regions/lines
