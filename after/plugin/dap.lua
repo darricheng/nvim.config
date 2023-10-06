e06157276833aa26c8c364fc8fdd16f7f4912e4a
Darric Heng <darricheng@gmail.com>
2023-10-06 19:28:51 +0800

    cleanup dap keymap code

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -70,15 +70,25 @@ dap.listeners.after.event_initialized['dapui_config'] = dapui.open
 dap.listeners.before.event_terminated['dapui_config'] = dapui.close
 dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
--- Basic debugging keymaps, feel free to change to your liking!
-vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Debug: Start/Continue' })
-vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
-vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
-vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
-vim.keymap.set('n', '<leader>B', function()
-  dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
-end, { desc = 'Debug: Set Breakpoint' })
+local map = function(keys, func, desc)
+  if desc then
+    desc = 'De[B]ugger: ' .. desc
+  end
+  if keys then
+    keys = '<leader>b' .. keys
+  end
+  vim.keymap.set('n', keys, func, { desc = desc })
+end
 
--- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-vim.keymap.set('n', '<F7>', dapui.toggle, { desc = 'Debug: See last session result.' })
+-- Basic debugging keymaps, feel free to change to your liking!
+map('s', dap.continue, '[S]tart/Continue')
+map('i', dap.step_into, 'Step [I]nto')
+map('v', dap.step_over, 'Step O[V]er')
+map('o', dap.step_out, 'Step [O]ut')
+map('b', dap.toggle_breakpoint, 'Toggle [B]reakpoint')
+map('B', function()
+  dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
+end, 'Set [B]reakpoint')
+
+-- dap-ui
+map('u', dapui.toggle, 'Toggle [U]I')
