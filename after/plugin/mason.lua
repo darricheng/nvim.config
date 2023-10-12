7bb91ab9fa2fbe52ece82b273065d0b434586ae2
Darric Heng <darricheng@gmail.com>
2023-10-12 21:16:14 +0800

    update mason ensure installed

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,13 +1,11 @@
 require('mason-tool-installer').setup {
   -- a list of all tools you want to ensure are installed upon
   -- start; they should be the names Mason uses for each tool
-  -- NOTE: LSPs are handled separately in lsp.lua
+  -- NOTE: LSPs are handled separately in lsp.lua, DAPs in dap.lua
+  -- TODO: Consolidate all the tool installation into this file?
   ensure_installed = {
-    -- go
-    'delve',
     -- javascript / typescript
     'prettierd',
-    'js-debug-adapter',
     -- lua
     'stylua',
     'luacheck',
