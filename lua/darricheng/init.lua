ad5f5407294a57cecb2263259250fb1f62117c43
Darric Heng <darric-sg@zto.com>
2023-11-20 17:20:48 +0800

    cleanup comments

--- lua/darricheng/init.lua
+++ lua/darricheng/init.lua
@@ -35,6 +35,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
-
--- The line beneath this is called `modeline`. See `:help modeline`
--- vim: ts=2 sts=2 sw=2 et
