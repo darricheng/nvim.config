0168ffa549f6617bac36744afc365df6357396bb
Darric Heng <darric-sg@zto.com>
2023-12-01 11:10:29 +0800

    use default cmdline position so that autocomplete shows tgt

--- after/plugin/editor.lua
+++ after/plugin/editor.lua
@@ -64,12 +64,4 @@ require('noice').setup {
     inc_rename = false, -- enables an input dialog for inc-rename.nvim
     lsp_doc_border = false, -- add a border to hover docs and signature help
   },
-  cmdline = {
-    opts = {
-      position = {
-        row = '40%',
-        col = '50%',
-      },
-    },
-  },
 }
