8cb1752c5d04d12d7e95a40b3bedc3a86c5b7855
Darric Heng <darricheng@gmail.com>
2024-02-04 09:46:01 +0800

    readd esc keymap for terminal

--- after/plugin/terminal.lua
+++ after/plugin/terminal.lua
@@ -2,3 +2,6 @@
 -- Be more familiar with the base terminal
 -- I should be able to do pretty much everything I want with it
 -- with the vi-mode of tmux
+
+-- Retain the esc map in case I need to use the vim terminal
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
